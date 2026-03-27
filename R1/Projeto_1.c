7d8221a3cf4aa917945e1a49afa95acb2d925830
Rossi8891 <enzogazirerossi@gmail.com>
2026-03-26 23:03:53 -0300

    primeiro commit

---
+++ R1/Projeto_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float num1;
+    float num2;
+    
+    scanf("%f", &num1);
+ 
+    scanf("%f", &num2);
+    
+    printf("%.2f", (num1 + num2)/2.0);
+    
+}
