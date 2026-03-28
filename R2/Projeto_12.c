c4f292e60c41bb2a201368e3c1e89ade9a12db24
Rossi8891 <enzogazirerossi@gmail.com>
2026-03-28 20:43:09 -0300

    novo commit

---
+++ R2/Projeto_12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+   
+   int valor;
+   scanf("%d", &valor);
+   
+   if (valor == 0){
+       printf("nulo");
+   }
+   
+    else if (valor > 0){
+       printf("positivo");
+   }
+   
+   else {
+       printf("negativo");
+   }
+   
+    
+    
+}
